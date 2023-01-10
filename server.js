eff4f435a4ae43114d073af2bb0135cf33b0076b
bassem eloraby <BASEM12683@GMAIL.COM>
2023-01-10 22:17:46 +0300

    mongo connect then listen

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const colors = require('colors');
 const connectDB = require('./config/db');
 const cors = require('cors')
 
-connectDB();
+// connectDB();
 const app = express()
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
@@ -14,10 +14,15 @@ app.use('/api/tasks', require('./routes/taskRaoutes'));
 
 const port = process.env.PORT || '5000'
 
-
-
-app.listen(port, err => {
-	if (err)
-		throw err
-	console.log('Server listening on port', port)
+//Connect to the database before listening
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log("listening for requests");
+    })
 })
+
+// app.listen(port, err => {
+// 	if (err)
+// 		throw err
+// 	console.log('Server listening on port', port)
+// })
